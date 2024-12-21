8f6d8810e3544bd47559e0bebef19799297b1972
Vipul Singh <104285752+Vipul123kumar@users.noreply.github.com>
2024-12-21 12:28:16 +0530

    js added

---
+++ index.js
@@ -0,0 +1,12 @@
+let finalvalue='0';
+document.querySelector('#display').value=finalvalue;
+function ab()
+{
+    console.log("enter");
+    
+   //let bval=document.getElementById('btn').innerText;
+  // finalvalue+=bval;
+   document.querySelector('#display').value= finalvalue;
+};
+
+//document.querySelector('btn').onclick=ab();
